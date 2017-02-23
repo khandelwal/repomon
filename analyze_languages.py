55eb6d38822aec934e7f2ac6983453743eaf8850
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2017-02-23 18:39:23 +0000

    Create simple bar chart for languages

--- analyze_languages.py
+++ analyze_languages.py
@@ -1,5 +1,9 @@
 import json
 from collections import defaultdict
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import seaborn as sns
 
 if __name__ == '__main__':
   with open('languages.json') as data_file:
@@ -10,6 +14,20 @@ if __name__ == '__main__':
 
     for repo in data:
       for lang, byte_count in repo['languages'].items():
-        print('{0},{1}'.format(lang, byte_count))
         lang_byte_count[lang] += int(byte_count) 
-    print(lang_byte_count)
+
+    lang_df = pd.DataFrame.from_dict(lang_byte_count, orient='index')
+    lang_df.columns = ['bytes']
+    lang_df['languages'] = lang_df.index
+    lang_df.sort_values('bytes',ascending=True, inplace=True)
+
+    sns_plot = sns.barplot(x='languages', y='bytes', data=lang_df,
+    estimator=sum)
+
+    for i in sns_plot.get_xticklabels():
+      i.set_rotation(90)
+
+    plt.ticklabel_format(style='plain', axis='y',useOffset=False)
+    plt.title('Languages by total bytes')
+
+    sns_plot.figure.savefig('langs.png', bbox_inches='tight')
